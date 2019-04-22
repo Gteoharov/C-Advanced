2d98cbf0e07fc26907a0892cad132be1aa319373
Gteoharov <georgia.teoharov@gmail.com>
2019-04-23 00:55:52 +0300

    enter new classes for excercises

---
+++ CarSalesman/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CarSalesman
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public double Weight { get; set; }
+
+        public string Color { get; set; }
+    }
+}
