224f2105c724ea5472c2773521fd4c7714407b43
Gteoharov <georgia.teoharov@gmail.com>
2019-05-03 08:43:08 +0300

    Generics

---
+++ Generics/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generics
+{
+    public class Box<T>
+    {
+        private List<T> value;
+
+        public Box()
+        {
+            this.value = new List<T>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.value.Count;
+            }
+        }
+
+        public void Add(T element)
+        {
+            this.value.Add(element);
+        }
+
+        public T Remove()
+        {
+            var last = this.value.Last();
+            this.value.RemoveAt(value.Count - 1);
+            return last;
+        }
+    }
+}
