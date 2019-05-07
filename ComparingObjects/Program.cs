a7d449b97ab795c51e6a2b39bc09a184e44c01fd
Gteoharov <georgia.teoharov@gmail.com>
2019-05-07 21:31:09 +0300

    IComparable

---
+++ ComparingObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComparingObjects
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
