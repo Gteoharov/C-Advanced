3aef5b6617e5beb992e1795090545521087d1b5e
Gteoharov <georgia.teoharov@gmail.com>
2019-05-07 15:32:26 +0300

    Interators

---
+++ InteratorsAndComparators/ListyIterator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InteratorsAndComparators
+{
+    public class ListyIterator<T>
+    {
+        private List<T> data;
+        private int index;
+
+        public ListyIterator(List<T> data)
+        {
+            this.data = data;
+            this.index = 0;
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                this.index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (index + 1 < this.data.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string Print()
+        {
+            return this.data[index].ToString();
+        }
+    }
+}
